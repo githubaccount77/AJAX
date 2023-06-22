e63403981a01b1076791b2e0743a9cb6b52cf695
githubaccount77 <christophertubman@outlook.com>
2023-06-22 11:37:16 -0400

    CT Changelog 6/22/23

---
+++ process.php
@@ -0,0 +1,22 @@
+<?php
+
+// Connect to database
+
+
+
+  echo 'Processing...';
+
+  //Check for GET variable
+
+  if(isset($_GET['name'])){
+    echo 'GET: Your name is '.$_GET['name'];
+    
+  }
+
+    //Check for POST variable
+
+    if(isset($_POST['name'])){
+      echo 'POST: Your name is '.$_POST['name'];
+      
+    }
+  
